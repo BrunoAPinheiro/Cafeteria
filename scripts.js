f13e3a57f28074be48af4e7708939c5d467bef44
BrunoAPinheiro <brunopinheiro14@hotmai.com>
2025-01-30 13:29:17 -0300

    Correção de responsividade p/telas pequenas, header alterado para hamburguer p/ telas pequenas, adicionado função de fechar modal com click fora do iframe.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector (".modal")
 const mascara = document.querySelector (".mascara-modal")
+const menu = document.getElementById ("menu-recolher")
 
 
 function mostrarModal() {
@@ -15,3 +16,30 @@ function esconderModal() {
     document.querySelector('.mascara-modal').style.visibility = 'hidden';
     document.querySelector('.mascara-modal').style.opacity = '0';
 }
+
+//Função para recolher o menu
+function recolherMenu(){
+    const menu = document.getElementById("nav-menu");
+    menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+}
+
+//fecha o menu quando o clique for fora do menu ou do botão de hambúrguer
+document.addEventListener("click", function (clickNoBody){
+    const menu = document.getElementById("nav-menu");
+    const menuRecolher = document.getElementById("menu-recolher");
+
+//Verifica se o clique foi fora do menu e do botão hambúrguer
+    if (!menu.contains(clickNoBody.target) && !menuRecolher.contains(clickNoBody.target)) {
+        menu.style.display = "none"; //fecha o menu
+    }
+});
+
+// Fecha o modal se o clique for fora do iframe (dentro do modal)
+mascara.addEventListener("click", esconderModal);
+// Verifica se o clique foi fora do iframe dentro do modal
+modal.addEventListener("click", function (clickNoBody){
+
+    if (!clickNoBody.target.closest("iframe")) {
+        esconderModal();
+    }
+});
